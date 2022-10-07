6a101dcc381e26e564e6d33f1ee6e8f1232aff4d
dimeprog <dimejiawoyefa5@gmail.com>
2022-10-07 09:50:25 +0100

    set_up reusablewidget

--- study_app/lib/screens/introduction/app_introduction.dart
+++ study_app/lib/screens/introduction/app_introduction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AppIntroductionScreen extends StatelessWidget {
   const AppIntroductionScreen({super.key});
@@ -7,8 +8,15 @@ class AppIntroductionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text('yydyyugdgdsg'),
+        child: Column(
+          children: [
+            Icon(Icons.star),
+            SizedBox(
+              height: Get.width * 0.2,
+            ),
+            Text(
+                'This is a study App. You can use it it as you like, if you  understand how it works you have to scale it. With this you will be able to master firrebase backend and flutter frontend'),
+          ],
         ),
       ),
     );

97b831d649ea15cf4c0fd5d874bb38e94de380d2
dimeprog <dimejiawoyefa5@gmail.com>
2022-10-07 09:07:55 +0100

    add ignore file

--- study_app/lib/screens/introduction/app_introduction.dart
+++ study_app/lib/screens/introduction/app_introduction.dart
@@ -5,6 +5,12 @@ class AppIntroductionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('yydyyugdgdsg'),
+        ),
+      ),
+    );
   }
 }
